d8c3606e3e2dbe9fadb91035dec77b93fc0646e8
IpastorSan <ignaciopastorsan@gmail.com>
2022-05-22 13:39:24 +0200

    desktop roadmap completed

--- src/components/CountdownTimer/CountDownTimerElements.js
+++ src/components/CountdownTimer/CountDownTimerElements.js
@@ -5,5 +5,23 @@ export const TimerWrapper = styled.div`
 `
 
 export const Timer = styled.p `
-    
+    font-family: "fleshandblood";
+    font-size: 56px;
+    padding-bottom: 0;
+    margin-bottom: 0;
+
+    @media screen and (max-width: 1800px){
+       
+    }
+
+    @media screen and (max-width: 1600px){
+       
+    }
+    @media screen and (max-width: 1400px){
+        font-size: 36px;
+    }
+
+    @media screen and (max-width: 1200px){
+        
+    }
 `
--- src/components/RoadMap/index.js
+++ src/components/RoadMap/index.js
@@ -19,25 +19,25 @@ function RoadMap() {
       <TimerWrapper >
         <CountDownTimer daysHoursMinSecs={daysHoursMinSecs} />
       </TimerWrapper>
-      <Subtitle></Subtitle>
+      <Subtitle>"This is just the beginning"</Subtitle>
       <RoadMapStagesWrapper>
         <RoadMapCardContainer>
           <h3>11:00 UTC</h3>
           <img src={point} alt="point" />
-          <img src={line} alt="line" />
+          <img src={line} alt="line" style={{margin: "1rem 0"}}/>
           <StageCardWrapper>Hotbit IEO <br/> Bounce/wbstarter IDO</StageCardWrapper>
         </RoadMapCardContainer>
         <RoadMapCardContainer>
           <h3>12:00 UTC</h3>
           <img src={point} alt="point" />
-          <img src={line} alt="line" />
+          <img src={line} alt="line" style={{margin: "1rem 0"}} />
           <StageCardWrapper>Sales Launch Date <br/> NFT Reveal Date</StageCardWrapper>
         </RoadMapCardContainer>
         <RoadMapCardContainer>
           <h3>13:00 UTC</h3>
           <img src={point} alt="point" />
-          <img src={line} alt="line" />
-          <StageCardWrapper>NFT Reveal Date</StageCardWrapper>
+          <img src={line} alt="line" style={{margin: "1rem 0"}}/>
+          <StageCardWrapper>NFT Reveal Date <br /> Original Collection</StageCardWrapper>
         </RoadMapCardContainer>
       </RoadMapStagesWrapper>
       {/* <RoadMapImage src = {countdownImage}/> */}
