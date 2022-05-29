d7e4d553c336847cd1e35e8b55f54709ba846f65
IpastorSan <ignaciopastorsan@gmail.com>
2022-05-29 23:31:34 +0200

    New gallery version

---
+++ src/data/galleryDataLarge.js
@@ -0,0 +1,77 @@
+import galleryImage1 from "../images/gallery1.png"
+import galleryImage2 from "../images/gallery3.png"
+import galleryImage3 from "../images/gallery5.png"
+import galleryImage4 from "../images/gallery6.png"
+import galleryImage5 from "../images/gallery2.png"
+import galleryImage6 from "../images/gallery9.png"
+import galleryImage7 from "../images/gallery4.png"
+import galleryImage8 from "../images/gallery7.png"
+import galleryImage9 from "../images/gallery10.png"
+import galleryImage10 from "../images/gallery8.png"
+import galleryImage11 from "../images/gallery11.png"
+import galleryImage12 from "../images/gallery12.png"
+import galleryImage13 from "../images/gallery13.png"
+import galleryImage14 from "../images/gallery14.png"
+
+
+export const galleryDataLarge = [
+    {
+    image: galleryImage1,
+},
+
+{
+    image: galleryImage3,
+},
+
+{
+    image: galleryImage2,
+},
+
+{ 
+    image: galleryImage4,
+},
+
+{
+    image: galleryImage5,
+},
+
+{
+    image: galleryImage6,
+},
+
+{
+    image: galleryImage7,
+},
+
+{
+    image: galleryImage8,
+},
+
+{
+    image: galleryImage9,
+},
+
+{
+    image: galleryImage10,
+},
+
+{
+    image: galleryImage11,
+},
+
+{
+
+    image: galleryImage12,
+},
+
+{
+
+    image: galleryImage13,
+},
+
+{
+
+    image: galleryImage14,
+},
+
+]
---
+++ src/data/galleryDataMedium.js
@@ -0,0 +1,54 @@
+import galleryImage1 from "../images/gallery1.png"
+import galleryImage2 from "../images/gallery3.png"
+import galleryImage3 from "../images/gallery5.png"
+import galleryImage4 from "../images/gallery6.png"
+import galleryImage5 from "../images/gallery2.png"
+import galleryImage6 from "../images/gallery9.png"
+import galleryImage7 from "../images/gallery4.png"
+import galleryImage8 from "../images/gallery7.png"
+import galleryImage9 from "../images/gallery10.png"
+import galleryImage10 from "../images/gallery8.png"
+
+
+export const galleryDataMedium = [
+    {
+    image: galleryImage1,
+},
+
+{
+    image: galleryImage3,
+},
+
+{
+    image: galleryImage2,
+},
+
+{ 
+    image: galleryImage4,
+},
+
+{
+    image: galleryImage5,
+},
+
+{
+    image: galleryImage6,
+},
+
+{
+    image: galleryImage7,
+},
+
+{
+    image: galleryImage8,
+},
+
+{
+    image: galleryImage9,
+},
+
+{
+    image: galleryImage10,
+},
+
+]
